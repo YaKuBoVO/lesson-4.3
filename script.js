d53d7b0f180cb46bf2077ed99d0a1d89c475aed1
YaKuBoVO <oyakubov526@gmail.com>
2023-06-23 23:17:37 +0500

    erataga tayyor boladi

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 "use strict"
+const btnTitle = document.querySelector('#btn-title');
+const elSee = document.querySelector('#see');
+const elShow = document.querySelector('#show');
+
+const elDisplayNone = document.querySelector('#display-none')
+
+const elButBtn = document.querySelector('#but-btn');
+const elSubBtn = document.querySelector('#sub-btn');
+
+elButBtn.style.display = 'none';
+elSubBtn.style.display = 'none';
+
+
+
+
+//btnTitle.addEventListener('click', (e) => {
+//   if (elSubBtn.style.display = 'none') {
+//     elSubBtn.style.display = 'block';
+//   } else if(elSubBtn.style.display = 'none') {
+//      elShow.textContent = "Choose"
+//   }
+//});
+
+
+
+btnTitle.addEventListener('click', (e) => {
+   
+});
